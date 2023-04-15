6557ef1b522d5f82500c3c61df4b306c924399fb
Krunal Gupta <guptakrunal02@gmail.com>
2023-04-15 18:18:33 +0530

    updated

--- Add_two_matrices.c
+++ Add_two_matrices.c
@@ -1,34 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    int a[3][3],b[3][3],c[3][3],i,j;
+    int a[3][3], b[3][3], c[3][3], i, j;
 
     printf("Enter 1st matrix elements:\n");
 
-    for(i=0;i<3;i++)
+    for (i = 0; i < 3; i++)
     {
-        for(j=0;j<3;j++)
+        for (j = 0; j < 3; j++)
         {
-            scanf("%d",&a[i][j]);
+            scanf("%d", &a[i][j]);
         }
     }
-    
+
     printf("\nEnter 2nd matrix elements:\n");
 
-    for(i=0;i<3;i++)
+    for (i = 0; i < 3; i++)
     {
-        for(j=0;j<3;j++)
+        for (j = 0; j < 3; j++)
         {
-            scanf("%d",&b[i][j]);
+            scanf("%d", &b[i][j]);
         }
     }
 
-    for(i=0;i<3;i++)
+    for (i = 0; i < 3; i++)
     {
-        for(j=0;j<3;j++)
+        for (j = 0; j < 3; j++)
         {
-            
+            printf(c[i][j] = a[i][j] + b[i][j]);
         }
     }
 
