6d83e04b700fd9a7738e3a14d38d446b6308d426
Krunal Gupta <guptakrunal02@gmail.com>
2023-04-15 18:24:56 +0530

    push

--- Insertion_Sort.c
+++ Insertion_Sort.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
 // Function to print an array
-void printArray(int array[], int size) {
-  for (int i = 0; i < size; i++) {
+void printArray(int array[], int size)
+{
+  for (int i = 0; i < size; i++)
+  {
     printf("%d ", array[i]);
   }
   printf("\n");
 }
 
-void insertionSort(int array[], int size) {
-  for (int step = 1; step < size; step++) {
+void insertionSort(int array[], int size)
+{
+  for (int step = 1; step < size; step++)
+  {
     int key = array[step];
     int j = step - 1;
 
@@ -17,7 +21,8 @@ void insertionSort(int array[], int size) {
     // it is found.
     // For descending order, change key<array[j] to key>array[j].
 
-    while (key < array[j] && j >= 0) {
+    while (key < array[j] && j >= 0)
+    {
       array[j + 1] = array[j];
       --j;
     }
@@ -26,7 +31,8 @@ void insertionSort(int array[], int size) {
 }
 
 // Driver code
-int main() {
+int main()
+{
   int data[] = {9, 5, 1, 4, 3};
   int size = sizeof(data) / sizeof(data[0]);
   insertionSort(data, size);
